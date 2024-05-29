9062d957dd3d662f5f5b12da1767baf1929545f5
shubhyadav4838 <shubhyadav4838@gmail.com>
2024-05-29 16:47:07 +0530

    Create smtp connection

---
+++ Day 32 Birthday wisher/create_connection.py
@@ -0,0 +1,13 @@
+import smtplib
+
+password = "[Enter Your App Password]"
+my_email = "shubhyadav753@gmail.com"
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls() #Transport Layer Security
+
+    connection.login(user = my_email, password = password) #Login to the email account using a secure connection (TLS/SSL).
+    connection.sendmail(
+        from_addr = my_email, 
+        to_addrs = "shubhyadav4838@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of my email")
+    
